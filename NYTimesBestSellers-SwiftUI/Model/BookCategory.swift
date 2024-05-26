5756f0e335b751360e788362a4e98f752a5ebb6d
Farhana <farhana.swe@gmail.com>
2024-05-26 16:32:09 -0700

    Create BookCategory.swift

---
+++ NYTimesBestSellers-SwiftUI/Model/BookCategory.swift
@@ -0,0 +1,14 @@
+//
+//  BookCategory.swift
+//  NYTimesBestSellers-SwiftUI
+//
+//  Created by Farhana Mustafa on 5/26/24.
+//
+
+import Foundation
+
+enum BookCategory: String, CaseIterable {
+    case graphicBooksAndManga = "graphic-books-and-manga"
+    case manga = "manga"
+    case travel = "travel"
+}
