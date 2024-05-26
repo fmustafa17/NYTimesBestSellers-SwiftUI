4bd59525b8e1193884da66f17a6e269d90d8af83
Farhana <farhana.swe@gmail.com>
2024-05-26 16:45:16 -0700

    Create BookCategoryListView

---
+++ NYTimesBestSellers-SwiftUI/View/BookCategoryListView.swift
@@ -0,0 +1,18 @@
+//
+//  BookCategoryListView.swift
+//  NYTimesBestSellers-SwiftUI
+//
+//  Created by Farhana Mustafa on 5/26/24.
+//
+
+import SwiftUI
+
+struct BookCategoryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookCategoryListView()
+}
