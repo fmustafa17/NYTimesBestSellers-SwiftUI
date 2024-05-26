ac7b90f03352c8256625644554afe25d316b6055
Farhana <farhana.swe@gmail.com>
2024-05-26 16:05:08 -0700

    start new project

---
+++ NYTimesBestSellers-SwiftUI/NYTimesBestSellers_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTimesBestSellers_SwiftUIApp.swift
+//  NYTimesBestSellers-SwiftUI
+//
+//  Created by Farhana Mustafa on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct NYTimesBestSellers_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
