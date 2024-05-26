cd80ee21f3b2ee335da175c4b0f381b1c0f5c615
Farhana <farhana.swe@gmail.com>
2024-05-26 16:33:15 -0700

    Create BookViewModel.swift

---
+++ NYTimesBestSellers-SwiftUI/ViewModel/BookViewModel.swift
@@ -0,0 +1,89 @@
+//
+//  BookViewModel.swift
+//  NYTimesBestSellers-SwiftUI
+//
+//  Created by Farhana Mustafa on 5/26/24.
+//
+
+import Combine
+import Foundation
+
+class BookViewModel: ObservableObject {
+    
+    // MARK: - Combine
+    @Published var booksResults: BookListResults?
+    
+    func fetchBookData() {
+        let category = randomBookCategory
+        
+        self.fetchBookListResults(with: category,
+                                  successHandler: { [weak self] (books) in
+            self?.booksResults = books
+        }, errorHandler: { (error) in
+            print(error)
+        })
+    }
+    
+    private func fetchBookListResults(with category: String,
+                                      successHandler: @escaping (BookListResults) -> Void,
+                                      errorHandler: @escaping (Error) -> Void) {
+        let baseURL = "https://api.nytimes.com/svc/books/v3/lists/current/"
+        let apiKey = Config.apiKey.rawValue
+        let fullURLString = baseURL + category + ".json?api-key=" + apiKey
+        
+        guard let url = URL(string: fullURLString) else {
+            print("Couldn't retrieve a URL with \(fullURLString)")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                print("Error encounted:", error?.localizedDescription ?? "")
+                return
+            }
+            
+            guard let data = data else {
+                print("Something went wrong with data")
+                return
+            }
+            
+            do {
+                let bookListResults = try JSONDecoder().decode(BookListResults.self,
+                                                               from: data)
+                successHandler(bookListResults)
+            } catch {
+                errorHandler(error)
+            }
+        }
+        task.resume()
+    }
+    
+    // MARK: - Async Await
+    func fetchBooksWithAsyncAwait() async throws -> BookListResults {
+        let category = randomBookCategory
+        let baseURL = "https://api.nytimes.com/svc/books/v3/lists/current/"
+        let apiKey = Config.apiKey.rawValue
+        let fullURLString = baseURL + category + ".json?api-key=" + apiKey
+        
+        guard let url = URL(string: fullURLString) else {
+            print("Couldn't retrieve a URL with \(fullURLString)")
+            throw NYTimesError.invalidURL
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let bookListResults = try JSONDecoder().decode(BookListResults.self,
+                                                       from: data)
+        return bookListResults
+    }
+    
+    var randomBookCategory: String {
+        return BookCategory.allCases.randomElement()?.rawValue ?? "manga"
+    }
+    
+    enum NYTimesError: Error {
+        case invalidURL
+    }
+}
