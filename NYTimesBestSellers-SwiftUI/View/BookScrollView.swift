068be5ad61ff8f3afc0c82feb891f611f5812ae3
Farhana <farhana.swe@gmail.com>
2024-05-26 17:48:03 -0700

    Update BookScrollView.swift

--- NYTimesBestSellers-SwiftUI/View/BookScrollView.swift
+++ NYTimesBestSellers-SwiftUI/View/BookScrollView.swift
@@ -10,9 +10,20 @@ import SwiftUI
 /// Reusable component
 /// Horizontal scrolling for all the best sellers for the given category
 struct BookScrollView: View {
+    @StateObject var viewModel: BookViewModel = BookViewModel()
+
     var body: some View {
         ScrollView(.horizontal) {
-            Text("Hello")
+            if let books = viewModel.books {
+                ForEach(books, id: \.id) { book in
+                    let urlString = book.bookImage ?? ""
+                    AsyncImage(url: URL(string: urlString))
+                    
+                }
+            }
+        }
+        .task {
+            viewModel.fetchBookData()
         }
     }
 }
